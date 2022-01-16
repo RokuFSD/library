5f9235288d711ee2e4664f61bc13ed6e10a111d0
RokuFSD <emilianordx@gmail.com>
2022-01-15 21:57:27 -0300

    starting storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 let myBooks = [];
 
 class Book {
+
   constructor(title, author, numPages, readed) {
     this.title = title;
     this.author = author;
@@ -10,22 +11,38 @@ class Book {
     this.readed = readed;
   }
 
-  getInfo = () => {
-    return `${this.title} by ${this.author}, ${this.numPages} pages, ${
-      this.readed ? "readed" : "not readed"
-    }`;
-  };
+  setReaded = (readed) => {
+    this.readed = readed;
+  }
+
+  getTitle = () => {
+    return this.title;
+  }
+
 }
+
 
 /*================ Variables ===================*/
 
 const cardsContainer = document.querySelector(".main");
 const addButton = document.querySelector("#btn-add");
 const submitButton = document.querySelector(".btn-sub");
-const formContainer = document.querySelector(".formAdd");
 const form = document.querySelector(".form");
+const formContainer = document.querySelector(".formAdd");
+const bgText = document.querySelector(".bg-text");
 
 /*================= Functions ====================*/
+const loadUserData = () => {
+  let currentBooks = localStorage.getItem("myArray");
+  if(currentBooks == null) return;
+  currentBooks = JSON.parse(currentBooks);
+  const storedBooks = currentBooks.map(book => {
+    new Book(book.title, book.author, book.numPages, book.readed);
+  });
+  console.log(storedBooks);
+
+  updateGrid();
+}
 
 const addBookToLibrary = (book) => {
   myBooks.push(book);
@@ -33,23 +50,38 @@ const addBookToLibrary = (book) => {
 
 const printBook = (book) => {
   const card = document.createElement("ARTICLE");
-  const h2 = document.createElement("H2");
+  const h1 = document.createElement("H1");
   const h3 = document.createElement("H3");
   const p = document.createElement("P");
-  h2.innerText = book.title;
-  h3.innerText = book.author;
-  p.innerText = book.numPages;
-  card.append(h2);
+  const buttonRemove = `<button class="card__btn card__btn--remove">Remove</button>`;
+  let doc;
+
+  if (book.readed) {
+    doc = `<button class="card__btn" id="btn__readed">Readed <i class="material-icons">check</i></button>`;
+  } else {
+    doc = `<button class="card__btn card__btn--false" id="btn__readed">Readed <i class="material-icons">close</i></button>`;
+  }
+
+  h1.innerText = book.title;
+  h3.innerText = `Author: ${book.author}`;
+  p.innerText = `Pages: ${book.numPages}`;
+  card.append(h1);
   card.append(h3);
   card.append(p);
+  card.insertAdjacentHTML("beforeend", doc);
+  card.insertAdjacentHTML("beforeend", buttonRemove);
   card.classList.add("card");
   cardsContainer.append(card);
 };
 
 const updateGrid = () => {
-  for (let book of myBooks){
+  cardsContainer.innerHTML = "";
+  myBooks.length > 0 ? bgText.innerHTML = "" : bgText.innerHTML = "No books added";
+  for (let book of myBooks) {
     printBook(book);
   }
+  let myArraySerialized = JSON.stringify(myBooks);
+  localStorage.setItem("myArray", myArraySerialized);
 };
 
 const createBook = () => {
@@ -59,19 +91,48 @@ const createBook = () => {
   let readedState = document.querySelector("#readed").checked;
   let book = new Book(title, author, qtyPages, readedState);
   addBookToLibrary(book);
-}
-
-const verifyInput = () => {
-  return document.querySelector("#pages").value < 1 ? alert("ERROR! Verify number of pages") : true;
 };
 
-const clearForm = () =>{
-  form.reset();
-}
+const verifyInput = () => {
+  return document.querySelector("#pages").value < 1
+    ? alert("ERROR! Verify number of pages")
+    : true;
+};
 
-const closeForm = () =>{
+const clearForm = () => {
+  form.reset();
+};
+
+const closeForm = () => {
   formContainer.classList.remove("formAdd-show");
   formContainer.classList.add("formAdd-hide");
+};
+
+const checkButton = (button) => {
+  return button.classList.contains("card__btn--false")
+    ? "not readed"
+    : "readed";
+};
+
+const updateStatus = (button) => {
+  myBooks.filter((book) => {
+    if (book.getTitle().toUpperCase() !== button.parentNode.firstChild.innerText) return;
+
+    if (checkButton(button) == "readed") {
+      button.childNodes[1].innerHTML = "check";
+      book.setReaded(true);
+    } else {
+      button.childNodes[1].innerHTML = "close";
+      book.setReaded(false);
+    }
+  });
+};
+
+const removeBook = (card) => {
+  card.remove();
+  let newBooks = myBooks.filter(book => book.getTitle() !== card.firstChild.innerHTML);
+  myBooks = [...newBooks];
+  updateGrid();
 }
 
 /*================= Events ================*/
@@ -88,9 +149,21 @@ formContainer.addEventListener("click", (e) => {
 });
 
 submitButton.addEventListener("click", () => {
-  if(verifyInput()){
+  if (verifyInput()) {
     createBook();
     clearForm();
     closeForm();
+    updateGrid();
   }
 });
+
+cardsContainer.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON" && e.target.id === "btn__readed") {
+    e.target.classList.toggle("card__btn--false");
+    updateStatus(e.target);
+  }
+
+  if(e.target.tagName === "BUTTON" && e.target.classList.contains("card__btn--remove")) removeBook(e.target.parentNode);
+});
+
+loadUserData();
